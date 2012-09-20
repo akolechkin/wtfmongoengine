abf3e5aa073643497b60cf9d58503de8decea4b5
Anton Kolechkin <anton.kolechkin@gmail.com>
2012-09-21 03:10:46 +0700

    no coerce, using force unicode for choices keys

--- wtfmongoengine/forms.py
+++ wtfmongoengine/forms.py
@@ -81,8 +81,7 @@ class DocumentFieldConverter(object):
             kwargs['validators'].append(validators.Optional())
 
         if document_field.choices:
-            kwargs['choices'] = document_field.choices
-            kwargs['coerce'] = unicode
+            kwargs['choices'] = ((unicode(k), v) for k, v in document_field.choices)
             return fields.SelectField(**kwargs)
 
         document_field_type = type(document_field).__name__
